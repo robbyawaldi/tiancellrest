16c4f7d6f81e44bf10c4dc321ae8f16ebe50cec4
robbyawaldi <robbyawaldi@gmail.com>
2019-08-17 09:18:11 +0700

    tambah setting session

--- tiancell/settings.py
+++ tiancell/settings.py
@@ -128,3 +128,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
