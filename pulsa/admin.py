6fcac41cee5325c74e98e3c4cfda31ba8673ce5c
robbyawaldi <robbyawaldi@gmail.com>
2019-08-26 20:43:56 +0700

    tambah app pulsa ke admin

--- pulsa/admin.py
+++ pulsa/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from pulsa.models import Provider, Nominal, Transaction
 
-# Register your models here.
+admin.site.register(Provider)
+admin.site.register(Nominal)
+admin.site.register(Transaction)
