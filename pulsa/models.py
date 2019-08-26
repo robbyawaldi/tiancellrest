c76679c0a1ed9923687df9205d118e4deb032bd6
robbyawaldi <robbyawaldi@gmail.com>
2019-08-26 20:35:09 +0700

    tambah app pulsa

---
+++ pulsa/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+class Nominal(models.Model):
+    provider = models.ForeignKey(
+        Provider,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=15)
+    cost = models.FloatField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return '%s %s / cost:%d / price:%d' % (
+            self.provider,
+            self.name,
+            self.cost,
+            self.price,
+        )
+
+
+class Transaction(models.Model):
+    nominal = models.ForeignKey(
+        Nominal,
+        on_delete=models.CASCADE
+    )
+    cost = models.FloatField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return '%s %s / cost:%d / price:%d' % (
+            self.nominal.provider,
+            self.nominal.name,
+            self.cost,
+            self.price,
+        )
--- tiancell/settings.py
+++ tiancell/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rangefilter',
     'stocks',
     'services',
+    'pulsa',
 ]
 
 MIDDLEWARE = [
