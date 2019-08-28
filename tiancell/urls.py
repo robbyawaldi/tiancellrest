4d4b534ed567dfb4aed0c9944e83e41dba589a8d
robbyawaldi <robbyawaldi@gmail.com>
2019-08-28 07:47:07 +0700

    ubah url admin

--- tiancell/urls.py
+++ tiancell/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/stocks/', include('stocks.urls')),
     path('api/services/', include('services.urls')),
     path('api-auth/', include('rest_framework.urls')),
