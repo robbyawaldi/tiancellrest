ec2a1e642e65b7c2efe5f953d61d2407b087c199
robbyawaldi <robbyawaldi@gmail.com>
2019-09-01 15:08:39 +0700

    fix panjang nama item di barcode

--- stocks/models.py
+++ stocks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=17)
     price = models.FloatField()
 
     def purchases(self):
