3ae4b6fddca8cc88926ad68c7ea3f200e11d84de
robbyawaldi <robbyawaldi@gmail.com>
2019-09-01 11:07:09 +0700

    custom header admin

--- tiancell/urls.py
+++ tiancell/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Tian Cell'
+admin.site.index_title = 'Adminsitration'
+admin.site.site_title = 'Home'   
+
 urlpatterns = [
     path('', admin.site.urls),
     path('api/stocks/', include('stocks.urls')),
